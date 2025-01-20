1f11db50a2072567b39a7b349656707e763555a2
Joost <joost@raeder-123.de>
2025-01-20 17:47:32 +0100

    Add solution for Q5.b

--- Assignment 2/distributed_data_processing/haters.py
+++ Assignment 2/distributed_data_processing/haters.py
@@ -10,12 +10,17 @@ class Haters(MRJob):
 
     def mapper(self, _, line: str):
         i, user_id, movie_id, rating, timestamp, rating_normalized = line.split(',')
-        # TODO
-        ...
+        yield user_id, (float(rating), 1)
+
 
     def reducer(self, key, values):
-        # TODO
-        ...
+        negative_ratings = 0
+        for rating, x in values:
+            if rating < 2.0:
+                negative_ratings += x
+        if negative_ratings >= 50:
+            yield None, key
+
 
 
 if __name__ == '__main__':
